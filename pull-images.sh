4d20a85407f44573beb81b27eab8545b641b72df
Peter Mikus <pmikus@cisco.com>
2025-03-16 10:01:43 +0100

    feat(hugo): Switch theme

---
+++ pull-images.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Home
+wget --no-clobber --content-disposition --directory-prefix=content "https://drscdn.500px.org/photo/1109502553/q%3D90_m%3D2048/v2?sig=593cc44469ca2d2fcf1a064f7f77cd84849424111d2719f9858a20c4a619d08f"

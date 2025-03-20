c091d770b8652b55580fbc8520c0ba35ac0628ac
Peter Mikus <pmikus@cisco.com>
2025-03-20 19:28:21 +0100

    feat(hugo): fix the images

--- content/travel/index.md
+++ content/travel/index.md
@@ -5,12 +5,12 @@ title: Travel
 categories: ["travel"]
 weight: 3
 params:
-  featured_image: castle-by-500em
+  featured_image: castle-by-500em.jpg
   theme: dark
   sort_order: desc
   sort_by: Name # Exif.Date
 resources:
-  - src: castle-by-500em
+  - src: castle-by-500em.jpg
     params:
       cover: true
 ---
--- pull-images.sh
+++ pull-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Home
-wget --no-clobber --content-disposition --directory-prefix=content "https://drscdn.500px.org/photo/1109502553/m%3D4096/v2?sig=8b795a9231da086534035f545f345a750b941ecad224256b1ac1a7c1f299f71f" -O castle-by-500em.jpg
+wget --no-clobber --content-disposition --directory-prefix=content "https://cvws.icloud-content.com/B/AU33vP4L9GaK2F-OI7yBqs4HXzOBAQTZPRXx1TN1sdtb3uCbSeDbmuNR/castle-by-500em.jpeg?o=AiBcUrwTfT9QhNnEXNBXJKtsUoOi8yDaQbTflxqixog8&v=1&x=3&a=CAogbhoXEi9ylx2h-Qbj10GhfJ6p8kSR8d9INexC0GMS0kwSaxDrgbWm2zIY696QqNsyIgEAUgQHXzOBWgTbmuNRaiV2jiuxRbjVEpJFc1nb61j5gWCG3YLlL-BgFBRsPovo3QVGBNQgciU569Kn2sqd6cdQqKrEEyBZOAvQ8u13jYrXeGJOazi0hTRxnccW&e=1742498705&fl=&r=3df52cbd-13cc-4cbf-adc2-c634ce68a655-1&k=FIu--LNnjt1ZsfSp5jAb7Q&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=178&s=gREPIrhMFPvNqKosHL7yhZPPLII&cd=i" -O castle-by-500em.jpg
 
 # Travel
-wget --no-clobber --content-disposition --directory-prefix=content/travel "https://drscdn.500px.org/photo/1109502553/m%3D4096/v2?sig=8b795a9231da086534035f545f345a750b941ecad224256b1ac1a7c1f299f71f" -O castle-by-500em.jpg
+wget --no-clobber --content-disposition --directory-prefix=content/travel "https://cvws.icloud-content.com/B/AU33vP4L9GaK2F-OI7yBqs4HXzOBAQTZPRXx1TN1sdtb3uCbSeDbmuNR/castle-by-500em.jpeg?o=AiBcUrwTfT9QhNnEXNBXJKtsUoOi8yDaQbTflxqixog8&v=1&x=3&a=CAogbhoXEi9ylx2h-Qbj10GhfJ6p8kSR8d9INexC0GMS0kwSaxDrgbWm2zIY696QqNsyIgEAUgQHXzOBWgTbmuNRaiV2jiuxRbjVEpJFc1nb61j5gWCG3YLlL-BgFBRsPovo3QVGBNQgciU569Kn2sqd6cdQqKrEEyBZOAvQ8u13jYrXeGJOazi0hTRxnccW&e=1742498705&fl=&r=3df52cbd-13cc-4cbf-adc2-c634ce68a655-1&k=FIu--LNnjt1ZsfSp5jAb7Q&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=178&s=gREPIrhMFPvNqKosHL7yhZPPLII&cd=i" -O castle-by-500em.jpg
